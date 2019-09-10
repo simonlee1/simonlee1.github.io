e68b79be7cb4a9aad1392b328da3179191fd1c7e
Simon Lee <simonl11@uci.edu>
2019-09-09 23:47:25 -0700

    Debugging purposes

--- JavaScript/visualizer.js
+++ JavaScript/visualizer.js
@@ -1,10 +1,39 @@
-function drawGraph(){
+function drawGraph(visualizerData){
 
+  for(i=0; i < visualizerData["array"].length; i++){
+    var width = parseInt(window.innerWidth, 10)/visualizerData["array"].length;
+    var newItem = `
+      <div style='height: ${visualizerData["array"][i]}px;width:1;background-color: #555'>
+      </div>
+    `
+
+    $("#arrayHolder").append(newItem);
+  }
 }
 
 function initializeVisualizer(visualizerData){
   visualizerData["algorithm"] = "bubble";
-  visulizerData["size"] = 50;
+  visualizerData["size"] = 50;
+  visualizerData["array"] = generateRandomArray(50);
+}
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+function generateRandomArray(size){
+  var array = [];
+  for(i = 1; i <= size; i++){
+    array.push(i);
+  }
+  shuffleArray(array);
+
+  return array
 }
 
 function getValue(visualizerData){
