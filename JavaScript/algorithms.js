fb189922a77c45583baae60d28bbb2d5e9b60d73
Simon Lee <simonl11@uci.edu>
2019-09-10 16:09:42 -0700

    Testing bubble sort

--- JavaScript/algorithms.js
+++ JavaScript/algorithms.js
@@ -1,12 +1,1 @@
-function bubbleSort(visualizerData){
-  var array = visualizerData["array"];
-  for (i = 0; i <array.length; i++){
-    for (j = 0; j < array.length-i-1;j++){
-      if array[j] > array[j+1]{
-        var temp = array[j];
-        array[j] = array[j+1];
-        array[j+1] = temp;
-      }
-    }
-  }
-}
+
