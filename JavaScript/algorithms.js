6385d102e5d2222950df7dd71d2327326bd8be11
Simon Lee <simonl11@uci.edu>
2019-09-25 14:08:55 -0700

    Completed Version 1.0 of the visualizer

--- JavaScript/algorithms.js
+++ JavaScript/algorithms.js
@@ -1,1 +1,118 @@
+function swap(data, index1, index2){
+  var temp = data[index1];
+  data[index1] = data[index2];
+  data[index2] = temp;
+}
 
+function bubbleSort(data){
+  var switches = [];
+  for (k = 0; k < data.length; k++){
+    for (j = 0; j < data.length-k-1; j++){
+      if (data[j] > data[j+1]){
+        switches.push([j, j+1]);
+        swap(data, j, j+1);
+      }
+    }
+  }
+  return switches;
+}
+
+function selectionSort(data){
+  var switches = [];
+  for (i = 0; i < data.length; i++){
+    var lowest = 100000;
+    var lowestIndex = -1;
+    for (j = i; j < data.length; j++){
+      if(data[j] < lowest){
+        lowest = data[j];
+        lowestIndex = j;
+      }
+    }
+    switches.push([i,lowestIndex]);
+    swap(data, i, lowestIndex);
+  }
+
+  return switches
+}
+
+function selectMedian(array, low, high){
+  var midIndex = low + Math.floor((high-low)/2);
+  if ((array[low] < array[midIndex] && array[low] > array[high]) || (array[low] > array[midIndex] && array[low] < array[high])){
+    return low;
+  }
+  else if((array[midIndex] > array[low] && array[midIndex] < array[high]) || (array[midIndex] < array[low] && array[midIndex] > array[high]) ){
+    return midIndex;
+  }
+  else{
+    return high;
+  }
+
+}
+
+function partition(array, low, high, switches){
+  var pivotIndex = selectMedian(array, low, high);
+  var pivot = array[pivotIndex];
+  switches.push([pivotIndex, high]);
+  swap(array, pivotIndex, high);
+
+  j = low - 1;
+  for (i = low; i < high; i++){
+    if(array[i] < pivot){
+      j++;
+      switches.push([i,j]);
+      swap(array, i, j);
+    }
+  }
+
+  switches.push([j + 1, high]);
+  swap(array, j + 1, high);
+  return j + 1;
+}
+
+function quickSort(array, low, high, switches){
+  if(low < high){
+    var partitionIndex = partition(array, low, high, switches);
+    quickSort(array, low, partitionIndex - 1, switches);
+    quickSort(array, partitionIndex + 1, high, switches);
+  }
+}
+
+function insert(array,index1, index2, switches){
+  swap(array, index1, index2);
+  switches.push([index1, index2]);
+
+  for (i = index1 + 1; i <= index2; i++){
+    swap(array,i, index2);
+    switches.push([i,index2]);
+  }
+
+}
+
+function merge(array, left, mid, right, switches){
+  var i = left;
+  var j = mid + 1;
+
+  while(j < right + 1 && i < right + 1){
+    if(array[i] < array[j]){
+      i++;
+    }
+    else{
+      insert(array, i, j, switches);
+      j++;
+    }
+  }
+
+}
+
+function mergeSort(array, left, right, switches){
+  if(left < right){
+    var mid = left + Math.floor((right-left)/2);
+
+    mergeSort(array, left, mid, switches);
+    mergeSort(array,mid +1, right, switches);
+
+    merge(array, left, mid, right, switches);
+  }
+
+
+}
--- JavaScript/visualizer.js
+++ JavaScript/visualizer.js
@@ -12,10 +12,61 @@ function drawGraph(currentData){
   }
 }
 
+function clearUnderlines(){
+  elements = document.getElementsByName("algorithm");
+
+  for (i = 0; i < elements.length; i++){
+    elements[i].style.textDecoration = "none";
+  }
+}
+
+function getSpeedWeight(algorithm){
+  var speedWeight;
+  if (algorithm == "bubble"){
+    speedWeight = 100;
+  }
+  else if(algorithm == "select"){
+    speedWeight = 150;
+  }
+  else if (algorithm == "quick"){
+    speedWeight = 150;
+  }
+  else if (algorithm == "merge"){
+    speedWeight = 100;
+  }
+  return speedWeight;
+}
+
+function updateOptions(algorithm, curElement){
+  if(curElement != undefined){
+    clearUnderlines();
+    curElement.style.textDecoration = "underline";
+  }
+  var speedWeight = getSpeedWeight(algorithm);
+  visualizerData["algorithm"] = algorithm;
+  visualizerData["size"] = parseInt($("#arraySize").val()) + 10;
+  visualizerData["array"] = generateRandomArray(parseInt($("#arraySize").val()) + 10);
+  visualizerData["speed"] = speedWeight - parseInt($("#arraySize").val())
+
+  if("id" in visualizerData){
+    clearInterval(visualizerData["id"]);
+  }
+
+  drawGraph(visualizerData);
+  document.getElementById("start").setAttribute("onClick", "run();");
+}
+
+function generateNewGraph(){
+  updateOptions(visualizerData["algorithm"]);
+  drawGraph(visualizerData);
+  document.getElementById("start").setAttribute("onClick", "run();");
+}
+
 function initializeVisualizer(data){
   data["algorithm"] = "bubble";
   data["size"] = 60;
   data["array"] = generateRandomArray(60);
+  data["speed"] = 100;
 }
 
 function shuffleArray(array) {
@@ -37,5 +88,97 @@ function generateRandomArray(size){
   return array
 }
 
-function getValue(visualizerData){
+function getListOfElements(){
+  var array = document.getElementsByClassName("arrayElement");
+  var returnable = [];
+
+  for (var i = 0; i < array.length; i++){
+    returnable.push(parseInt(array[i].getAttribute("arrayvalue")));
+  }
+
+  return returnable;
+}
+
+
+function switchElement(array, index1, index2){
+  var tempHeight = array[index1].style.height;
+  var tempValue = array[index1].arrayValue;
+  var tempBackgorundColor = array[index1].style.backgroundColor;
+  array[index1].style.height = array[index2].style.height;
+  array[index1].arrayValue = array[index2].arrayValue
+  array[index1].style.backgroundColor = array[index2].style.backgroundColor;
+  array[index2].style.height = tempHeight;
+  array[index2].arrayValue = tempValue;
+  array[index2].style.backgroundColor = tempBackgorundColor;
+}
+
+function clearPrevHighlight(curIndex, switches, array){
+  if(curIndex != 0){
+    prevIndex1 = switches[curIndex-1][0];
+    prevIndex2 = switches[curIndex-1][1];
+    array[prevIndex1].style.backgroundColor = "#555";
+    array[prevIndex2].style.backgroundColor = "#555";
+  }
+}
+
+function highlight(array, index1, index2, curIndex, switches){
+
+  clearPrevHighlight(curIndex, switches, array);
+
+  if (curIndex < switches.length){
+    array[index1].style.backgroundColor = "rgb(0,255,0)";
+    array[index2].style.backgroundColor = "rgb(255,0,0)";
+  }
+
+}
+
+function retrieveSwitches(array){
+  if (visualizerData["algorithm"] == "bubble"){
+    return bubbleSort(array);
+  }
+  else if(visualizerData["algorithm"] == "select"){
+    return selectionSort(array);
+  }
+  else if (visualizerData["algorithm"] == "quick"){
+    var switches = [];
+    quickSort(array, 0, array.length - 1, switches);
+    return switches;
+  }
+  else if(visualizerData["algorithm"] == "merge"){
+    var switches = [];
+    mergeSort(array, 0, array.length -1, switches);
+    return switches;
+  }
+  else{
+    return [];
+  }
+}
+
+function run(){
+  var array = getListOfElements();
+  var switches = retrieveSwitches(array);
+
+  var id = setInterval(frame, visualizerData["speed"]);
+  visualizerData["id"] = id;
+  var curIndex = 0;
+  var elements = document.getElementsByClassName("arrayElement")
+  highlight(elements, switches[curIndex][0], switches[curIndex][1], curIndex, switches)
+  document.getElementById("start").setAttribute("onClick", "");
+
+  function frame() {
+    if (curIndex >= switches.length) {
+      clearPrevHighlight(curIndex, switches, elements);
+      clearInterval(id);
+    } else {
+      switchElement(elements, switches[curIndex][0], switches[curIndex][1]);
+      curIndex += 1;
+    }
+    if (curIndex < switches.length){
+      highlight(elements, switches[curIndex][0], switches[curIndex][1], curIndex, switches);
+    }
+
+  }
+
+
+
 }
